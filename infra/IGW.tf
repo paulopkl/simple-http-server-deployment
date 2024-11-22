27d3e646f981603f757b71f6843f2480a7b240da
paulopkl <palmeida.ipms@gmail.com>
2024-11-22 12:49:23 -0300

    changed

--- infra/IGW.tf
+++ infra/IGW.tf
@@ -5,3 +5,22 @@ resource "aws_internet_gateway" "gateway" {
     Name = "main-gateway"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "main-public-route-table"
+  }
+}
+
+# Associate the route table with a subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
